b0bf4e2f8c9e300ccf1984d50fff5fbe099b18d8
Israel Gutierrez <igtz.castillo@gmail.com>
2025-03-23 21:46:18 +0200

    Initial Commit

---
+++ OuranRecorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OuranRecorder
+//
+//  Created by Israel Gutiérrez Castillo on 23.3.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OuranRecorder/OuranRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  OuranRecorderApp.swift
+//  OuranRecorder
+//
+//  Created by Israel Gutiérrez Castillo on 23.3.2025.
+//
+
+import SwiftUI
+
+@main
+struct OuranRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
